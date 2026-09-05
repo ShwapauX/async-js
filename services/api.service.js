5e896ce87d1aa0b6065a8762e2408aa096596ef1
Christopher BM <bamc1715@gmail.com>
2026-09-04 18:07:29 -0600

    pruebas de apis

--- services/api.service.js
+++ services/api.service.js
@@ -5,6 +5,9 @@
  * 
  * !!Super importante
  * !Todas las funciones asincronas retornan una promesa, no importa que pienses tu que esta retornando es una promesa
+ * 
+ * Nota
+ * La api tetch como el navegador por defecto lanza una peticion GET
  */
 
 import { API_URL } from "/env.js";
@@ -27,12 +30,66 @@ export const getSingleProduct = async function(id){
     try{
         const response = await fetch(`${API_URL}/${id}`)
         const data = await response.json();
-        console.log(data);
+        // console.log(data);
+        return data;
     }catch(error){
         console.error("Error", error);
     }
 };
 
-getSingleProduct(1);
-getSingleProduct(9);
-getSingleProduct(15);
+// POST
+export const createProduct = async (productObject) => {
+    try{
+        const response = await fetch(API_URL,{
+            method: "POST",
+            headers: {"Content-Type":"application/json"} ,
+            body: JSON.stringify(productObject),
+
+        })
+        const data = await response.json();
+        // console.log(data);
+        return data;
+    }catch(error){
+        console.error("Error", error);
+    }
+}
+
+createProduct({
+title: "Chips fuego",
+price: 20.0,
+description: "Es vida pura",
+category: "frituras",
+image: "http://example.com"
+})
+
+
+
+// PUT
+export async function updateProduct  (id, productObject) {
+    try{
+        const response = await fetch(`${API_URL}/${id}`,{
+            method: "PUT",
+            headers: {"Content-Type":"application/json"} ,
+            body: JSON.stringify(productObject),
+        })
+        const data = await response.json();
+        console.log(data);
+        // return data;
+    }catch(error){
+        console.error("Error", error);
+    }
+}
+updateProduct(10,{title:"Regulador de voltaje", price: 200})
+
+// DELETE
+export const deleteProduct = async function (id) {
+    try {
+    const response = await fetch(`${API_URL}/${id}`,{
+    method: "DELETE",
+    });
+    const data = await response.json();
+    console.log(data);
+    } catch (error) {
+        console.error("Error", error);
+    }
+};
